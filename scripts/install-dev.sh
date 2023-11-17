c7ae60188b03c0fcdb4a2f3e701a6dfc8492d62a
Vincent Emonet <vincent.emonet@gmail.com>
2023-11-17 13:36:36 +0100

    improve scripts

--- scripts/install-dev.sh
+++ scripts/install-dev.sh
@@ -5,13 +5,14 @@ set -e
 # Script to install dependencies for development and enable pre-commit hooks
 # Make sure you have enabled a venv before running this script
 
-pip install maturin pre-commit
+python3 -m venv .venv
+source .venv/bin/activate
 
-cargo install mdbook
+pip install maturin pre-commit
 
 rustup update
 rustup component add rustfmt clippy
 
-cargo install wasm-pack
+cargo install wasm-pack mdbook
 
 pre-commit install
