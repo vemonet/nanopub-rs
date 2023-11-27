bd2b5bb192cc369719c7db9aa0682eb557beea32
Vincent Emonet <vincent.emonet@gmail.com>
2023-11-27 20:16:04 +0100

    Fix np server URLs to use HTTPS. Bump to 0.0.13

--- lib/src/constants.rs
+++ lib/src/constants.rs
@@ -1,12 +1,12 @@
 /// List of available Nanopub servers
 /// Checkout the live list at https://monitor.petapico.org
 pub const LIST_SERVERS: [&str; 6] = [
-    "http://server.nanopubs.lod.labs.vu.nl/",
-    "http://server.np.dumontierlab.com/",
-    "http://app.tkuhn.eculture.labs.vu.nl/nanopub-server-1",
-    "http://app.tkuhn.eculture.labs.vu.nl/nanopub-server-2",
-    "http://app.tkuhn.eculture.labs.vu.nl/nanopub-server-3",
-    "http://app.tkuhn.eculture.labs.vu.nl/nanopub-server-4",
+    "https://server.nanopubs.lod.labs.vu.nl/",
+    "https://server.np.dumontierlab.com/",
+    "https://app.tkuhn.eculture.labs.vu.nl/nanopub-server-1",
+    "https://app.tkuhn.eculture.labs.vu.nl/nanopub-server-2",
+    "https://app.tkuhn.eculture.labs.vu.nl/nanopub-server-3",
+    "https://app.tkuhn.eculture.labs.vu.nl/nanopub-server-4",
 ];
 
 /// Nanopub test server URL
